aeab3bdca4fd32ab5e3599027496167ce2a25f0a
teddyrendahl <trendahl@slac.stanford.edu>
2018-04-19 11:58:51 -0700

    STY: Various PEP8 violations
    setup.py:4:11: E251 unexpected spaces around keyword / parameter equals
    setup.py:4:13: E251 unexpected spaces around keyword / parameter equals
    setup.py:5:14: E251 unexpected spaces around keyword / parameter equals
    setup.py:5:16: E251 unexpected spaces around keyword / parameter equals
    setup.py:6:15: E251 unexpected spaces around keyword / parameter equals
    setup.py:6:17: E251 unexpected spaces around keyword / parameter equals
    setup.py:7:14: E251 unexpected spaces around keyword / parameter equals
    setup.py:7:16: E251 unexpected spaces around keyword / parameter equals
    setup.py:8:13: E251 unexpected spaces around keyword / parameter equals
    setup.py:8:15: E251 unexpected spaces around keyword / parameter equals
    setup.py:9:15: E251 unexpected spaces around keyword / parameter equals
    setup.py:9:17: E251 unexpected spaces around keyword / parameter equals
    setup.py:10:18: E251 unexpected spaces around keyword / parameter equals
    setup.py:10:20: E251 unexpected spaces around keyword / parameter equals
    setup.py:11:6: E124 closing bracket does not match visual indentation

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 import versioneer
-from setuptools import (setup, find_packages)
+from setuptools import setup, find_packages
 
-setup(name     = 'nabs',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      license  = 'BSD',
-      author   = 'SLAC National Accelerator Laboratory',
-
-      packages    = find_packages(),
-      description = 'Not a Beamline Scientist',
-
-    )
+setup(name='nabs',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      license='BSD',
+      author='SLAC National Accelerator Laboratory',
+      packages=find_packages(),
+      description='Not a Beamline Scientist')
