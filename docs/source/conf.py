cb3b20420f5bac4ecd8abc32e9dc2462a328ec83
teddyrendahl <trendahl@slac.stanford.edu>
2018-04-19 11:54:42 -0700

    DEV: Add autosummary and napoleon Sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon'
 ]
+
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
