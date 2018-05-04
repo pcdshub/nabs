f7d83c098427f269633541bca93a1cde70f06d2c
teddyrendahl <trendahl@slac.stanford.edu>
2018-05-04 11:40:46 -0700

    DEV: Hardware and RunEngine fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from bluesky import RunEngine
+import pytest
+
+
+@pytest.fixture(scope='function')
+def RE(request):
+    loop = asyncio.new_event_loop()
+    loop.set_debug(True)
+    RE = RunEngine({}, loop=loop)
+
+    def clean_event_loop():
+        if RE.state != 'idle':
+            RE.halt()
+        ev = asyncio.Event(loop=loop)
+        ev.set()
+        loop.run_until_complete(ev.wait())
+
+    request.addfinalizer(clean_event_loop)
+    return RE
+
+
+@pytest.fixture(scope='function')
+def hw():
+    from ophyd.sim import hw
+    return hw()
