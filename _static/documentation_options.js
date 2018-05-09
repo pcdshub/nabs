6656eb9b08fc7ccfeb96490834b0c61a0d43d98e
Travis CI User <travis@example.org>
2018-05-09 21:19:58 +0000

    Update docs after building Travis build 9 of pcdshub/nabs
    The docs were built from the branch 'master' against the commit
    8ee10460e19b63a6351f26713c1fc7eed297239d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/nabs/jobs/377010740.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.19.gb7bcb3d',
+    VERSION: '0.0.0+34.g8ee1046',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
