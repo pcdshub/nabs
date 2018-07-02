d179e6a3cb33969b1ab94e96f073ab4b4aa469e3
teddyrendahl <trendahl@slac.stanford.edu>
2018-07-02 16:06:17 -0700

    ENH: ErrorSignal
    Also, add trigger methods to the DerivedSignal instances

--- nabs/utils.py
+++ nabs/utils.py
@@ -1,4 +1,5 @@
 from ophyd.signal import DerivedSignal
+from ophyd.sim import SignalRO
 
 
 class InvertedSignal(DerivedSignal):
@@ -24,3 +25,38 @@ class InvertedSignal(DerivedSignal):
     def inverse(self, value):
         """Invert the value"""
         return -value
+
+    def trigger(self):
+        return self.derived_from.trigger()
+
+
+class ErrorSignal(SignalRO, DerivedSignal):
+    """
+    Signal that reports the absolute error from a provided target
+
+    Parameters
+    ----------
+    derived_from : ophyd.Signal
+
+    target : float
+        Position of zero error
+    """
+    def __init__(self, derived_from, target, *, name=None, **kwargs):
+        # Create a name if None is given
+        if not name:
+            name = derived_from.name + '_error'
+        # Store the target
+        self.target = target
+        # Initialize the DerivedSignal
+        super().__init__(derived_from, name=name, **kwargs)
+
+    def forward(self, value):
+        """Invert the value"""
+        return NotImplemented
+
+    def inverse(self, value):
+        """Invert the value"""
+        return abs(value - self.target)
+
+    def trigger(self):
+        return self.derived_from.trigger()
